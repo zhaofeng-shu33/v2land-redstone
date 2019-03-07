409d78ef64a6e8108dd0c6ca588aad921604c00a
zhaofeng-shu33 <616545598@qq.com>
2019-03-07 20:47:21 +0800

    add new db-news-source sql, not finished

---
+++ scripts/db-news-source.sql
@@ -0,0 +1,14 @@
+
+CREATE TYPE public.enum_media_role AS ENUM (
+    'traditional',
+    'social media',
+    'institution'
+);
+CREATE TABLE public.media (
+    name text,
+    description text,
+    icon text,
+    type public.enum_media_role,
+    id integer NOT NULL
+);
+
